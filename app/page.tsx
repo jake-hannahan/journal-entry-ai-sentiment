9323bc6c21aa9bd990aa8256319ec5d74ab27b27
Jake Hannahan <jakehannahan@gmail.com>
2023-08-24 17:47:47 -0600

    added journal page layout route, added EntryCard and NewEntryCard, added utils for auth and api

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
 import Link from "next/link";
+import { auth } from "@clerk/nextjs";
 
-export default function Home() {
+export default async function Home() {
+  const { userId } = await auth();
+  let href = userId ? "/journal" : "/new-user";
+
   return (
     <div className="w-screen text-white h-screen bg-black flex justify-center items-center">
       <div className="w-full max-w-[600px] mx-auto">
@@ -10,7 +13,7 @@ export default function Home() {
           This is an app for tracking your mood through your life
         </p>
         <div>
-          <Link href={"/journal"}>
+          <Link href={href}>
             <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
               get started
             </button>
