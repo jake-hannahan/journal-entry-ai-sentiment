4bde6f047a0040d48b83b2ef5b0ea896e522308c
Jake Hannahan <jakehannahan@gmail.com>
2023-08-25 15:48:53 -0600

    update journal entries added

--- utils/api.ts
+++ utils/api.ts
@@ -12,5 +12,23 @@ export const createNewEntry = async () => {
   if (res.ok) {
     const data = await res.json();
     return data.data;
+  } else {
+    throw new Error("Something went wrong on API server!");
+  }
+};
+
+export const updateEntry = async (id, content) => {
+  const res = await fetch(
+    new Request(createURL(`/api/journal/${id}`), {
+      method: "PATCH",
+      body: JSON.stringify({ content }),
+    })
+  );
+
+  if (res.ok) {
+    const data = await res.json();
+    return data.data;
+  } else {
+    throw new Error("Something went wrong on API server!");
   }
 };
