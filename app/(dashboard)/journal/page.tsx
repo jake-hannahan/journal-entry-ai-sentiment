2e02ba6678fd9fa5493d5624ab77fa0209bbca6e
Jake Hannahan <jakehannahan@gmail.com>
2023-08-28 12:53:46 -0600

    add openai, langchain, zod for prompting gpt3.5 turbo

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -1,5 +1,6 @@
 import EntryCard from "@/components/EntryCard";
 import NewEntryCard from "@/components/NewEntryCard";
+import { analyze } from "@/utils/ai";
 import { getUserByClerkId } from "@/utils/auth";
 import { prisma } from "@/utils/db";
 import Link from "next/link";
@@ -14,6 +15,10 @@ const getEntries = async () => {
       createdAt: "desc",
     },
   });
+
+  await analyze(
+    `Today was an ok day. I found a new coffee shop that was cool but then I got a flat tire. :)`
+  );
 
   return entries;
 };
